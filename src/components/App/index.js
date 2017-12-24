af5dd23694b690342c1c98f5fc7627ff198f33fc
Rus Hughes <russell.hughes@gmail.com>
2017-12-24 22:30:31 +0700

    code refactor and web3 dependencies

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,21 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './style.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <h2>Block Explorer</h2>
+        </div>
+        <div className="App-nav">
+          {/* Place holder */}
+        </div>
       </div>
     );
   }
 }
-
 export default App;
