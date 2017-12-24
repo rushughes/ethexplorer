d83c97792b3e80c1ddcbc297e1319b051a238d34
Rus Hughes <russell.hughes@gmail.com>
2017-12-24 22:34:56 +0700

    refactor and created block and home sections

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './style.css';
+import Block from './../Block';
+import Home from './../Home';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
@@ -11,7 +14,17 @@ class App extends Component {
           <h2>Block Explorer</h2>
         </div>
         <div className="App-nav">
-          {/* Place holder */}
+          <Router>
+            <div>
+              <Link to="/">Home</Link>
+              <Link to="/block">Block</Link>
+              <Route exact path="/" component={Home}/>
+              <Route exact path="/block" render={() => (
+                <h3>Please select a blockHash.</h3>
+              )}/>
+              <Route path="/block/:blockHash" component={Block}/>
+            </div>
+        </Router>
         </div>
       </div>
     );
